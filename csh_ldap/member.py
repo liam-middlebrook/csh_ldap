4d8f9d64051c08bfd1eb1459daa99c4309a0afe1
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-09-22 20:20:11 -0400

    implement CSHMember getattr

--- csh_ldap/member.py
+++ csh_ldap/member.py
@@ -3,12 +3,15 @@ import ldap
 class CSHMember:
     __con__ = None
     __search_val__ = None
+    __ldap_user_ou__ = "ou=Users,dc=csh,dc=rit,dc=edu"
 
     def __init__(self, con, search_val):
         self.__con__ = con
         self.__search_val__ = search_val
 
-        print(self.__con__.search_s('ou=Users,dc=csh,dc=rit,dc=edu',
-            ldap.SCOPE_SUBTREE,
-            "(entryUUID=%s)" % s,
-            ['cn']))
+    def __getattr__(self, key):
+        return self.__con__.search_s(
+                self.__ldap_user_ou__,
+                ldap.SCOPE_SUBTREE,
+                "(entryUUID=%s)" % self.__search_val__,
+                [key])
