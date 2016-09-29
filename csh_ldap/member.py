4ca180ac081fc01ab74ddf49a9a3b7bc87bd1c0c
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-09-29 17:12:18 -0400

    Fix Member SetAttribute
    Add Groups accessors to Member object

--- csh_ldap/member.py
+++ csh_ldap/member.py
@@ -1,27 +1,45 @@
 import ldap
 
 class CSHMember:
-    __con__ = None
-    __search_val__ = None
     __ldap_user_ou__ = "ou=Users,dc=csh,dc=rit,dc=edu"
-    __dn__ = None
 
     def __init__(self, con, search_val):
-        self.__con__ = con
-        self.__search_val__ = search_val
+        self.__dict__['__con__'] = con
+        self.__dict__['__search_val__'] = search_val
 
-        self.__dn__ = con.search_s(
+        self.__dict__['__dn__'] = con.search_s(
                 self.__ldap_user_ou__,
                 ldap.SCOPE_SUBTREE,
                 "(entryUUID=%s)" % search_val,
                 ['uid'])[0][0]
 
-    def __getattr__(self, key):
-        return self.__con__.search_s(
+
+    def get(self, key):
+        return self.__getattr__(key, as_list=True)
+
+
+    def groups(self):
+        return self.get('memberOf')
+
+
+    def in_group(self, group):
+        if 'cn' not in group:
+            group = "cn=" + group + ",ou=Groups,dc=csh,dc=rit,dc=edu"
+        return group in self.groups()
+
+
+    def __getattr__(self, key, as_list=False):
+        res = self.__con__.search_s(
                 self.__ldap_user_ou__,
                 ldap.SCOPE_SUBTREE,
                 "(entryUUID=%s)" % self.__search_val__,
                 [key])
+
+        if as_list:
+            return [v.decode('utf-8') for v in res[0][1][key]]
+        else:
+            return res[0][1][key][0].decode('utf-8')
+
 
     def __setattr__(self, key, value):
         ldap_mod = None
@@ -35,10 +53,10 @@ class CSHMember:
         if value is None or value == "":
             ldap_mod = ldap.MOD_DELETE
         elif exists[0][1] == {}:
-            ldap_mod = ldap.MOD_REPLACE
-        else:
             ldap_mod = ldap.MOD_ADD
+        else:
+            ldap_mod = ldap.MOD_REPLACE
 
         mod_attrs = [(ldap_mod, key, value)]
 
-        __con__.modify_s(__dn__, mod_attrs)
+        self.__con__.modify_s(self.__dn__, mod_attrs)
